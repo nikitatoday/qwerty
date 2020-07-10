3d50cbad65c4dd772cce8739834a27baa5cd4a68
Nik <to4kaplus@gmail.com>
2020-07-10 21:01:32 +0300

    fixed writeYourGenrs

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ showMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
     for (let i = 1; i <=3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`)
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
 }
 
